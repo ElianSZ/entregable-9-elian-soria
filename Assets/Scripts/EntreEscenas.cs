0e873b6e0fe56dc70ba27d45acefaba2e700c5a9
Elián Soria Zamora <eliansoria7@gmail.com>
2022-03-03 13:52:36 +0100

    Entregable 9

---
+++ Assets/Scripts/EntreEscenas.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EntreEscenas : MonoBehaviour
+{
+    public void BtwnNewScene()
+    {
+        SceneManager.LoadScene("Escena 2");
+    }
+}
